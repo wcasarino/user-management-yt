e2ee508a44c3a3baeac61ae1de751aa8b94df560
wcasarino <wcasarino@gmail.com>
2023-02-21 18:26:15 -0300

    Schema and routes

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,12 @@
+import userRouter from '#Routes/user.routes.js';
 import express from 'express';
 
 const expressApp = express();
+
+//Middlewares
+expressApp.use(express.json());
+
+//Routes
+expressApp.use('/user', userRouter);
 
 export default expressApp;
